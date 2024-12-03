jgunthorpe/cloud_mdir_sync

commit fba1436c914c0459fc514c5ea88b26819fa43418

office365: Add a AppRegistration lookup class for different tenants
Support baking various tenant registrations into cloud_mdir_sync and
looking up the most appropriate registration based on the user.

Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

cloud_mdir_sync/office365.py
@@ -79,6 +79,39 @@ def _retry_protect(func):
     return async_wrapper
 
 
+class AppRegistration():
+    registration_lookup: Dict[str,Dict[str,str]] = {
+        "nvidia.com": {
+            "tenant": "43083d15-7273-40c1-b7db-39efd9ccc17a",
+            "client_id": "472bb794-a546-4311-96f9-76bc79989214",
+        },
+    }
+    default_registration: Dict[str,str] = {
+        "tenant": "common",
+        "client_id": "122f4826-adf9-465d-8e84-e9d00bc9f234",
+    }
+
+    def __init__(self, user: str, use_default_registration: bool):
+        self.use_default_registration = use_default_registration
+        user_split = user.split("@")
+        if len(user_split) < 2:
+            self.use_default_registration = True
+        else:
+            self.domain = user_split[1]
+
+    def get(self) -> tuple[str,str]:
+        default_tenant = self.default_registration["tenant"]
+        default_client_id = self.default_registration["client_id"]
+
+        if self.use_default_registration:
+            return (default_tenant, default_client_id)
+
+        registration = self.registration_lookup.get(self.domain)
+        if registration is None:
+            return (default_tenant, default_client_id)
+
+        return (registration["tenant"], registration["client_id"])
+
 class GraphAPI(oauth.Account):
     """An OAUTH2 authenticated session to the Microsoft Graph API"""
     graph_token: Optional[Dict[str,str]] = None

cloud_mdir_sync/config.py
@@ -82,11 +82,15 @@ class Config(object):
     def all_mboxes(self):
         return itertools.chain(self.local_mboxes, self.cloud_mboxes)
 
-    def Office365_Account(self, user=None, tenant="common", client_id="122f4826-adf9-465d-8e84-e9d00bc9f234"):
+    def Office365_Account(self, user=None, tenant=None, client_id=None, use_default_registration=False):
         """Define an Office365 account credential. If user is left as None
         then the browser will prompt for the user and the choice will be
         cached. To lock the account to a single tenant specify the Azure
         Directory name, ie 'contoso.onmicrosoft.com', or the GUID."""
+        if tenant is None or client_id is None:
+            from .office365 import AppRegistration
+            app_registration = AppRegistration(user, use_default_registration)
+            tenant, client_id = app_registration.get()
         from .office365 import GraphAPI
         self.async_tasks.append(GraphAPI(self, user, tenant, client_id))
         return self.async_tasks[-1]

commit 27eca8901dbc047de4fb50ea1160a24eb87d6d91

Support standard collection type hinting generics in python<3.9
Python 3.7 and above offer a pathway to avoid depending on specialized type
hinting annotations for collections that were deprecated in python 3.9.
Import the annotation support from __future__ to keep the program
compatible with python>=3.7.

Fixes: fba1436c914c ("office365: Add a AppRegistration lookup class for different tenants")
Signed-off-by: Rahul Rameshbabu <sergeantsagara@protonmail.com>

cloud_mdir_sync/office365.py
@@ -1,4 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0+
+
+# Bring standard collection type hinting generics to python<3.9
+from __future__ import annotations
+
 import asyncio
 import datetime
 import functools